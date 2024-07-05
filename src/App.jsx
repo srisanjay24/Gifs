55d435169019236447360e4ee31e31605cd183d5
srisanjay24 <srisanjay2412@gmail.com>
2024-07-05 02:56:00 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './App.css'
+import AppLayout from './layouts/app-layout';
+import Home from './pages/home';
+import Search from './pages/search';
+import GifPage from './pages/single-gif';
+import Favorites from './pages/favorites';
+import GifProvider from './context/gif-context';
+import Category from './pages/category';
+
+
+
+  const router=createBrowserRouter([
+    {
+      element:<AppLayout/>,
+      children: [
+          {
+            path:"/",
+            element:<Home/>
+          },
+          {
+            path:"/:category",
+            element:<Category/>
+          },
+          {
+            path:"/search/:query",
+            element:<Search/>
+          },
+          {
+            path:"/:type/:slug",
+            element:<GifPage/>
+          },
+          {
+            path:"/favorites",
+            element:<Favorites/>
+          },
+      ]
+    },
+  ])
+  function App() {
+  return(
+  <GifProvider>
+  <RouterProvider router={router}/>
+  </GifProvider>
+  );
+}
+
+export default App
